51251f48db5c5eac22f4fe11c55fb2628430778c
Zachary Alex Stern <zacharyalexstern@gmail.com>
2012-04-29 21:57:08 -0400

    Created "base" class which includes the ssh module as a resource. Also, added base module to each node.

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -1,13 +1,21 @@
+class base {
+    include ssh
+}
+
 node 'puppet.example.com' {
+    include base
 }
 
 node 'web.example.com' {
+    include base
 }
 
 node 'db.example.com' {
+    include base
 }
 
 node 'mail.example.com' {
+    include base
 }
 
 
