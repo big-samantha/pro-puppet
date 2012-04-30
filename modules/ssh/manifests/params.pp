c6315f3cc9788d9696c7a4be1eea95418fb46b7b
Zachary Alex Stern <zacharyalexstern@gmail.com>
2012-04-29 20:21:39 -0400

    Added $ssh_service_config line to params for setting per-operating system location of sshd_config

--- modules/ssh/manifests/params.pp
+++ modules/ssh/manifests/params.pp
@@ -2,15 +2,21 @@ class ssh::params {
   case $operatingsystem {
     Solaris: {
       $ssh_package_name = 'openssh'
+      $ssh_service_config = '/etc/ssh/sshd_config'
     }
   }
     
     /(Ubuntu|Debian)/: {
     $ssh_package_name = 'openssh-server'
+    $ssh_service_config = '/etc/ssh/sshd_config'
   }
     
     /(RedHat|CentOS|Fedora)/: {
     $ssh_package_name = 'openssh-server'
+    $ssh_service_config = '/etc/ssh/sshd_config'
     }
   }
 }
+
+
+
