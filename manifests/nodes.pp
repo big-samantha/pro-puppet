ecb021077d29b0ee8a1a5c7ec3f7e7b4004de0e1
Zachary Alex Stern <zs@enternewmedia.com>
2012-05-11 18:38:02 +0000

    Added ssh module to the puppet nodes, which makes sure openssh server is installed.

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -1,13 +1,17 @@
 node 'puppet.example.com' {
+  include ssh
 }
 
 node 'web.example.com' {
+  include ssh
 }
 
 node 'db.example.com' {
+  include ssh
 }
 
 node 'mail.example.com' {
+  include ssh
 }
 
 
